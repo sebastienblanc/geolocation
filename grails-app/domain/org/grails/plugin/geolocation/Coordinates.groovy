f35bb3dfe4bdeb5dafcc17f84b8fee2abf1e0fb5
Gaurav <gchauhan@jellyfishtechnologies.com>
2012-01-19 19:44:36 +0530

    1. Add controllers,services, domain, taglib under the default packaging structure followed by grails plugins. 2.Fix for 404 error on Firefox and Chrome

--- grails-app/domain/org/grails/plugin/geolocation/Coordinates.groovy
+++ grails-app/domain/org/grails/plugin/geolocation/Coordinates.groovy
@@ -1,16 +1,6 @@
 package org.grails.plugin.geolocation
 
 class Coordinates {
-GeoPosition position
-    static constraints = {
-      latitude(nullable:true)
-      longitude(nullable:true)
-      altitude(nullable:true)
-      accuracy(nullable:true)
-      altitudeAccuracy(nullable:true)
-      heading(nullable:true)
-      speed(nullable:true)
-    }
     double latitude
     double longitude
     double altitude
@@ -18,5 +8,16 @@ GeoPosition position
     double altitudeAccuracy
     double heading
     double speed 
- static belongsTo = GeoPosition
+	
+    static belongsTo = [position:GeoPosition]
+	
+    static constraints = {
+	latitude(nullable:true)
+	longitude(nullable:true)
+	altitude(nullable:true)
+	accuracy(nullable:true)
+	altitudeAccuracy(nullable:true)
+	heading(nullable:true)
+	speed(nullable:true)
+    }  
 }
